c360a67c8ede8d6b87df6c86d858cc8ba8453608
mirko-mc <8547740+mirko-mc@users.noreply.github.com>
2024-07-22 21:13:45 +0200

    re-initial commit

---
+++ src/data/fetch.js
@@ -0,0 +1,18 @@
+const FETCHURL = "https://striveschool-api.herokuapp.com/api/profile/";
+// GET => RITORNA LA LISTA DEI PROFILI UTENTE https://striveschool-api.herokuapp.com/api/profile/
+// GET => RITORNA IL TUO PROFILO https://striveschool-api.herokuapp.com/api/profile/me
+// GET => RITORNA UN PROFILO SPECIFICO https://striveschool-api.herokuapp.com/api/profile/{userId}
+// PUT => AGGIORNA IL PROFILO UTENTE (EXTRA) https://striveschool-api.herokuapp.com/api/profile/
+// GET => RITORNA UNA LISTA DI ESPERIENZE https://striveschool-api.herokuapp.com/api/profile/:userId/experiences
+// POST => CREA UNA NUOVA ESPERIENZA https://striveschool-api.herokuapp.com/api/profile/:userId/experiences
+// GET => RITORNA UNA SPECIFICA ESPERIENZA (EXTRA) LEGATA AL PUT https://striveschool-api.herokuapp.com/api/profile/:userId/experiences/:expId
+// PUT => MODIFICA UNA SPECIFICA ESPERIENZA (EXTRA) https://striveschool-api.herokuapp.com/api/profile/:userId/experiences/:expId
+// DELETE => ELIMINA UNA SPECIFICA ESPERIENZA (EXTRA) https://striveschool-api.herokuapp.com/api/profile/:userId/experiences/:expId
+/**
+ * "role":"CTO",
+ * "company":"Strive",
+ * "startDate":"2019-06-16",
+ * "endDate":"2019-06-16", null se ancora in corso
+ * "description":"Doing stuff",
+ * "area":"Berlin",
+ */
