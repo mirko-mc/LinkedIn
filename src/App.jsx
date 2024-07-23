89b681c66278ae839900bb8b3fa70c4d72c81269
mirko-mc <8547740+mirko-mc@users.noreply.github.com>
2024-07-23 09:59:22 +0000

    basic structure of App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,44 @@
+import { Col, Container, Row } from 'react-bootstrap';
 import './App.css';
-import {cercaProfilo } from './data/fetch';
+import { cercaProfilo } from './data/fetch';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
 
   return (
-    <>
-    <h1>Sono la Home</h1>
-    <button onClick={() => cercaProfilo()} className='btn btn-primary'>Testa la fetch</button>
-    </>
-
+    <BrowserRouter>
+      <header>
+        <Container fluid>
+          <Row>
+            <Col className='border'>
+              <h2>HEADER</h2>
+            </Col>
+          </Row>
+        </Container>
+      </header>
+      <main>
+        <Container>
+          <Row>
+            <Col lg={9} className='border'>
+              <h1>Sono il PROFILE</h1>
+              <button onClick={() => cercaProfilo()} className='btn btn-primary'>Testa la fetch</button>
+            </Col>
+            <Col lg={3} className='border'>
+              <h2>ASIDE</h2>
+            </Col>
+          </Row>
+        </Container>
+      </main>
+      <footer>
+        <Container fluid>
+          <Row>
+            <Col className='border'>
+              <h2>FOOTER</h2>
+            </Col>
+          </Row>
+        </Container>
+      </footer>
+    </BrowserRouter>
   );
 }
 
