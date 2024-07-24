c3ce6e95af1d2ce3ce30928b66d9626a5c690faa
samu2704 <samuele.calabrese@studenti.unisalento.it>
2024-07-24 19:34:03 +0200

    footer con card

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import AsideCard from './components/AsideCard';
 import Footer from './components/Footer';
 function App() {
   return (
     <>
+    <AsideCard/>
     <Footer/>
     </>
   );
