faf646c939a3ef0bf4209ce4fc8606cf60dcfdf3
samu2704 <samuele.calabrese@studenti.unisalento.it>
2024-07-26 20:39:18 +0200

    sezioni aside

--- src/components/AsideCard.jsx
+++ src/components/AsideCard.jsx
@@ -1,24 +1,28 @@
 import { Card } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import "./AsideCard.css";
-import { IcoFollow } from "../assets/svg/IcoSvg";
+import { IcoFollow, IcoOmino } from "../assets/svg/IcoSvg";
 
-function AsideCard({ profilo }) {
+function AsideCard({ profilo, type }) {
   return (
     <>
-      <div className="d-flex">
+      <div className="d-flex borAside">
         <Card.Img className="img" variant="top" src={profilo.image}></Card.Img>
 
         <Card.Body>
-          <Card.Title>{`${profilo.name} ${profilo.surname}`}</Card.Title>
+          <Card.Title><h6 className="mb-0">{`${profilo.name} ${profilo.surname}`}</h6></Card.Title>
           <div className="item">
-            <p>{profilo.title}</p>
-            <p>{profilo.bio}</p>
+            <p className="m-0">{profilo.title}</p>
+            <p className="mb-1">{profilo.bio}</p>
           </div>
-          <Button variant="light">
+          {type==="segui" ? <Button className="mb-4 buttonAside" variant="light">
             <IcoFollow />
             Segui
-          </Button>
+          </Button> : <Button className="mb-4 buttonAside" variant="light">
+            <IcoOmino />
+            collegati
+          </Button>}
+          
         </Card.Body>
       </div>
     </>
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -3,6 +3,7 @@ import Experience from "../components/Experience";
 import GeneralInfo from "../components/GeneralInfo";
 import Aside from "../components/Aside";
 
+
 function Profile() {
   return (
     <Container>
